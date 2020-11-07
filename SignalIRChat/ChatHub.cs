88391139f09cb795d03eca861139253291ad5163
chisoftltd <chisoftltd@gmail.com>
2020-11-07 15:13:57 +0100

    Add project files.

---
+++ SignalIRChat/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalIRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            //Clients.All.hello();
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
